408a7e2a180f77d8b83c7dbf978ca1f5ac543f48
jiangyi-creator <1432866213@qq.com>
2026-05-16 17:05:48 +0800

    home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import Homepanel from './components/Homepanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,15 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <!-- 测试面板组件 -->
+  <Homepanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>
+      我是新鲜好物插槽
+    </div>
+  </Homepanel>
+  <Homepanel title="人气推荐" sub-title="人气推荐 好多商品">
+    <div>
+      我是人气推荐插槽
+    </div>
+  </Homepanel>
 </template>
