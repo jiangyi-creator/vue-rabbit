f42184c4b58eb6c9e5042e774a18bae2a087c466
jiangyi-creator <1432866213@qq.com>
2026-05-17 11:33:23 +0800

    激活状态显示以及分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -4,13 +4,14 @@ import { onMounted, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 // 轮播图
 import { getBannerAPI } from '@/apis/home';
+import GoodsItem from '../Home/components/GoodsItem.vue';
 
-const categoryDate = ref({})
+const categoryData = ref({})
 const route = useRoute()
 
 const getCategory = async () => {
   const res = await getCategoryAPI(route.params.id)
-  categoryDate.value = res.result
+  categoryData.value = res.result
 }
 
 onMounted(() => getCategory())
@@ -38,7 +39,7 @@ onMounted(() => getBanner())
       <div class="bread-container">
         <el-breadcrumb separator=">">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>{{ categoryDate.name }}</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
       <!-- 轮播图 -->
@@ -48,6 +49,25 @@ onMounted(() => getBanner())
             <img :src="item.imgUrl" alt="">
           </el-carousel-item>
         </el-carousel>
+      </div>
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="goods in item.goods" :goods="goods" :key="goods.id" />
+        </div>
       </div>
     </div>
   </div>
