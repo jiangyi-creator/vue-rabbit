cbd773dbbb0ae1fac410d27c2c225b568ccde5ea
jiangyi-creator <1432866213@qq.com>
2026-05-17 17:21:39 +0800

    详情热榜-基础组件封装

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import DetailHot from './components/DetailHot.vue';
 import { getDetail } from '@/apis/detail';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
@@ -123,7 +123,8 @@ onMounted(() => getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-
+              <DetailHot></DetailHot>
+              <DetailHot></DetailHot>
             </div>
           </div>
         </div>
