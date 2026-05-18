2b91c07204dfcb8aea123c4b2d36542c34162e25
jiangyi-creator <1432866213@qq.com>
2026-05-18 13:05:26 +0800

    图片预览-小图切换大图显示

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,8 @@ import DetailHot from './components/DetailHot.vue';
 import { getDetail } from '@/apis/detail';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+// 1.放大镜效果步骤1, 引入通用组件
+import ImageView from '@/components/ImageView/index.vue'
 
 const goods = ref({})
 const route = useRoute()
@@ -40,7 +42,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView></ImageView>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
