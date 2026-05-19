2f0805bf385c5a7a6fe02cb8c38dcb36a81391c0
jiangyi-creator <1432866213@qq.com>
2026-05-19 14:07:22 +0800

    登录-pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { ElInfiniteScroll } from 'element-plus'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 
@@ -14,8 +14,10 @@ import { lazyPlugin } from './directives'
 import { componentPlugin } from './components'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+app.use(pinia)
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate)
 app.use(router)
 app.use(lazyPlugin)
 app.directive('infinite-scroll', ElInfiniteScroll)
