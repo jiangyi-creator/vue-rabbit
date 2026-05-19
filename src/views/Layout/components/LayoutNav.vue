f0b6ab03013790bef51358b584923c05b3321cd4
jiangyi-creator <1432866213@qq.com>
2026-05-19 14:26:03 +0800

    登录-使用token适配多模板展示

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { useUserStore } from '@/stores/user'
+const useStore = useUserStore()
+
 
 </script>
 
@@ -6,8 +9,12 @@
   <nav class="app-topnav">
     <div class="container">
       <ul>
-        <template v-if="false">
-          <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
+        <!-- 多模板渲染 区分登录状态和非登录状态 -->
+
+
+        <!-- 适配思路：登录时显示第一块 非登录时显示第二块 是否有token -->
+        <template v-if="useStore.userInfo.token">
+          <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ useStore.userInfo.account }}</a></li>
           <li>
             <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
