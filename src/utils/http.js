43b94f91f0822579d1581109decab43e804e1fb3
jiangyi-creator <1432866213@qq.com>
2026-05-19 14:52:11 +0800

    登录-token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import { useUserStore } from '@/stores/user'
 
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -12,6 +13,13 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+  // 1.从pinia中获取token数据
+  const useStore = useUserStore()
+  const token = useStore.userInfo.token
+  if(token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  // 2.按照后端要求拼接字符串
   return config
 }, e => Promise.reject(e))
 
