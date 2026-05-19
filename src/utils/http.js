3fce392b5f5134628c94e8c4911d7ae001b1fc4a
jiangyi-creator <1432866213@qq.com>
2026-05-19 15:33:38 +0800

    登录-401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from '@/stores/user'
-
+import router from "@/router";
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 5000
@@ -25,11 +25,20 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const useStore = useUserStore()
   // 统一错误提示
   ElMessage({
     type: 'warning',
     message: e.response.data.message
   })
+
+  // 401token失效处理
+  // 1.清除本地用户数据
+  // 2.跳转到登录页面
+  if(e.response.status === 401) {
+    useStore.clearUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 
