272e60ee9fd4c146acb47e126f477e2711c1ca78
jiangyi-creator <1432866213@qq.com>
2026-05-23 11:57:21 +0800

    修复API：http 改为 https 解决混合内容拦截

--- src/utils/http.js
+++ src/utils/http.js
@@ -5,7 +5,7 @@ import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from '@/stores/userStore'
 import router from "@/router";
 const httpInstance = axios.create({
-  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+  baseURL: 'https://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 50000
 })
 
